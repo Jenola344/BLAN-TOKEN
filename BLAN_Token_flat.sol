9f2c0a74ce77b5cc7dbcd6cfd0ea92ed36773b84
jesutolaolusegun <jesutolaolusegun@gmail.com>
2025-11-12 23:26:36 +0000

    commit

---
+++ BLAN_Token_flat.sol
@@ -0,0 +1,7 @@
+Error: 
+      Truffle Flattener must be run inside a Truffle or Buidler project:
+      truffle.js, truffle-config.js, buidler.config.js, nor buidler.config.ts found
+    
+    at getProjectRoot (/home/user/BLAN-TOKEN/node_modules/truffle-flattener/index.js:158:11)
+    at async flatten (/home/user/BLAN-TOKEN/node_modules/truffle-flattener/index.js:176:32)
+    at async main (/home/user/BLAN-TOKEN/node_modules/truffle-flattener/index.js:245:3)
