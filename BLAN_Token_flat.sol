061bf0d3ea9e12292f9e99bbe6b2c1130c6764b0
jesutolaolusegun <jesutolaolusegun@gmail.com>
2025-11-12 23:35:36 +0000

    commit

--- BLAN_Token_flat.sol
+++ BLAN_Token_flat.sol
@@ -1,7 +1,4 @@
-Error: 
-      Truffle Flattener must be run inside a Truffle or Buidler project:
-      truffle.js, truffle-config.js, buidler.config.js, nor buidler.config.ts found
-    
-    at getProjectRoot (/home/user/BLAN-TOKEN/node_modules/truffle-flattener/index.js:158:11)
-    at async flatten (/home/user/BLAN-TOKEN/node_modules/truffle-flattener/index.js:176:32)
-    at async main (/home/user/BLAN-TOKEN/node_modules/truffle-flattener/index.js:245:3)
+Error: None of the sub-resolvers resolved "BLAN" location.
+    at ResolverEngine.<anonymous> (/home/user/BLAN-TOKEN/node_modules/@resolver-engine/core/build/src/resolverengine.js:35:23)
+    at Generator.next (<anonymous>)
+    at fulfilled (/home/user/BLAN-TOKEN/node_modules/@resolver-engine/core/build/src/resolverengine.js:4:58)
